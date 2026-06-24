3a9e6966a819040da3ea99b525dc6742b7ac6975
DeveloperPraphull <developer.praphull@gmail.com>
2026-06-24 16:12:48 +0530

    testing mcp server2

--- app/api/github_routes.py
+++ app/api/github_routes.py
@@ -1,16 +1,12 @@
 from fastapi import APIRouter
 
 from app.models.github_models import PushCodeRequest
-from app.services.github_service import push_branch
 from app.services.github_service import push_code
 
 router = APIRouter()
 
-
 @router.post("/github/push")
-def github_push(
-    req: PushCodeRequest
-):
+def github_push(req: PushCodeRequest):
 
     return push_code(
         req.branch_name,
